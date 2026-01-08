13c84903686790f729ff01911cbea90a1d9b77d0
Nick Fahey <nfahey@live.com.au>
2026-01-08 15:11:55 +1100

    setup test packages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // enables Jest-like syntax in Vitest test files
+    environment: 'jsdom', // tells the test runner system (Vitest, Jest) to be ready to run browser-related code
+    setupFiles: 'src/setupTests.js', // Perform any additional configuration before any tests run
+  },
 });
