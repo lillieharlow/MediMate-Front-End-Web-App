02bbcad76e6fd4931a9903b953e365573430d04c
Nick Fahey <nfahey@live.com.au>
2026-01-10 10:57:50 +1100

    feat: added create account card on /login

--- src/components/CreateAccountCard.jsx
+++ src/components/CreateAccountCard.jsx
@@ -1,7 +1,22 @@
+import { useNavigate } from 'react-router';
+import { Card } from '../style/componentStyles';
+
 export default function CreateAccountCard() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const navigate = useNavigate();
+  const handleCreateAccount = () => {
+    navigate('/signup');
+  };
+
+  return (
+    <Card data-testid="app-create-account-card">
+      <h3 data-testid="app-create-account-card-heading">Don't have a MediMate account?</h3>
+      <button
+        type="button"
+        onClick={handleCreateAccount}
+        data-testid="app-create-account-card-button"
+      >
+        Create Patient Account
+      </button>
+    </Card>
+  );
 }
