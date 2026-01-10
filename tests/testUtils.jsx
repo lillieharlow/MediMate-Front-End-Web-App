c949e01bb4f3796a1268fcaddc8f623eade7c877
Nick Fahey <nfahey@live.com.au>
2026-01-10 11:26:52 +1100

    test: add tests for create account card on /login. Update test configuration to fix saved localStorage between tests

--- tests/testUtils.jsx
+++ tests/testUtils.jsx
@@ -1,14 +1,14 @@
-import { render } from "@testing-library/react";
-import { BrowserRouter } from "react-router";
-import { AuthProvider } from "../src/contexts/AuthContext";
-import AppRoutes from "../src/router/AppRoutes";
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { AuthProvider } from '../src/contexts/AuthContext';
+import AppRoutes from '../src/router/AppRoutes';
 
-export const renderWithAll = (ui) => {
+export const renderWithAll = ui => {
   return render(
-    <BrowserRouter>
+    <MemoryRouter>
       <AuthProvider>
         <AppRoutes>{ui}</AppRoutes>
       </AuthProvider>
-    </BrowserRouter>
+    </MemoryRouter>,
   );
 };
