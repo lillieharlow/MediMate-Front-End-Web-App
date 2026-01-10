515297130437af7f383032d48c1c3c2c6bf6a42f
Nick Fahey <nfahey@live.com.au>
2026-01-10 12:32:54 +1100

    refactor: create getErrorReason function to extract api response errors into a string for display

--- src/api/common.js
+++ src/api/common.js
@@ -1,22 +1,39 @@
-import { API_BASE_URL } from './config';
+import { API_BASE_URL } from "./config";
 
 export const post = async (endpoint, body, sendToken = false) => {
   const headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   };
 
   // Attach bearer token to header if request is to a protected route
   if (sendToken) {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) headers.Authorization = `Bearer ${token}`;
   }
 
   const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
-    method: 'POST',
+    method: "POST",
     headers,
     body: JSON.stringify(body),
   });
   // TODO: error handling
   const responseData = await response.json();
   return responseData;
+};
+
+export const getErrorReason = (apiResponse) => {
+  const errorsString = Array.isArray(apiResponse.errors)
+    ? apiResponse.errors
+        .map((e) => e?.msg)
+        .filter(Boolean)
+        .join(", ")
+    : undefined;
+
+  return (
+    errorsString ||
+    apiResponse.error ||
+    apiResponse.name ||
+    apiResponse.message ||
+    "Unknown error"
+  );
 };
