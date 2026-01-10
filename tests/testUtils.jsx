374b000546a969334cb062abc3aeeb37f39f9f7d
Nick Fahey <nfahey@live.com.au>
2026-01-10 17:09:30 +1100

    test: update testUtils to allow configuring MemoryRouter pathname

--- tests/testUtils.jsx
+++ tests/testUtils.jsx
@@ -3,9 +3,13 @@ import { MemoryRouter } from 'react-router';
 import { AuthProvider } from '../src/contexts/AuthContext';
 import AppRoutes from '../src/router/AppRoutes';
 
-export const renderWithAll = ui => {
+export const renderWithAuth = ui => {
+  return render(<AuthProvider>{ui}</AuthProvider>);
+};
+
+export const renderWithAll = (ui, routerPath = '/') => {
   return render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={[routerPath]}>
       <AuthProvider>
         <AppRoutes>{ui}</AppRoutes>
       </AuthProvider>
