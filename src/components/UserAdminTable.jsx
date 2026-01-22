6641e6f8ef65c3a0d06662025a89fd257aff5633
Nick Fahey <nfahey@live.com.au>
2026-01-22 11:38:43 +1100

    feat: Add function button placeholders to user administration page

--- src/components/UserAdminTable.jsx
+++ src/components/UserAdminTable.jsx
@@ -1,10 +1,21 @@
+import DeleteProfileButton from "./button/DeleteProfileButton";
+import ManageProfileButton from "./button/EditProfileButton";
+
 export default function UserAdminTable({ users }) {
+  const onManageProfile = (profileId) => {
+    console.log(`I will manage profile: ${profileId}`);
+  };
+
+  const onDeleteProfile = (profileId) => {
+    console.log(`I will delete profile: ${profileId}`);
+  };
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Fname</th>
-          <th>Lname</th>
+          <th>First Name</th>
+          <th>Last Name</th>
           <th>Email</th>
           <th>Role</th>
           <th>Functions</th>
@@ -13,12 +24,21 @@ export default function UserAdminTable({ users }) {
       <tbody>
         {users.map((user) => {
           return (
-            <tr key={user.id}>
+            <tr key={user._id}>
               <td>{user.firstName}</td>
               <td>{user.lastName}</td>
               <td>{user.user.email}</td>
               <td>{user.user.userType.typeName}</td>
-              <td>Functions Go Here</td>
+              <td>
+                <ManageProfileButton
+                  profileId={user._id}
+                  onManage={onManageProfile}
+                />
+                <DeleteProfileButton
+                  profileId={user._id}
+                  onManage={onDeleteProfile}
+                />
+              </td>
             </tr>
           );
         })}
