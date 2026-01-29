b407e863f4ca40b0b82506eab9613d359a9f3740
Lillie <chapman.lillie@gmail.com>
2026-01-29 11:57:13 +1100

    feat: add UpdateBookingButton component

---
+++ src/components/button/UpdateBookingButton.jsx
@@ -0,0 +1,17 @@
+// Button to open the update booking popup for a booking
+import ActionButton from "./ActionButton";
+
+function UpdateBookingButton({ bookingId, onUpdate }) {
+  return (
+    <ActionButton
+      $bg="#2393ca"
+      $color="#000000"
+      onClick={() => onUpdate(bookingId)}
+      style={{ fontSize: "0.85rem" }}
+    >
+      Update Booking
+    </ActionButton>
+  );
+}
+
+export default UpdateBookingButton;
